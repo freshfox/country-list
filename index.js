6c97defe40c8f0515d5bd43cf1f6b56380253be8
Dominic Bartl <dominic.bartl.at@gmail.com>
2017-04-28 16:50:56 +0200

    Map countries to different format

---
+++ index.js
@@ -0,0 +1,19 @@
+const countries = require('world-countries/countries');
+const EU = ['AUT','BEL','BGR','CYP','CZE','DEU','DNK','ESP','EST','FIN','FRA','GBR','GRC','HRV','HUN','IRL','ITA','LTU','LUX','LVA','MLT','NLD','POL','PRT','ROU','SVK','SVN','SWE'];
+
+let mapped = countries.map((country) => {
+	return {
+		cca2: country.cca2,
+        ccn3: country.ccn3,
+        cca3: country.cca3,
+        currency: country.currency[0],
+        name: country.name.common,
+        in_eu: EU.indexOf(country.cca2) >= 0,
+        translations: {
+        	en: country.name.common,
+        	de: country.translations.deu.common
+        }
+	}
+});
+
+module.exports = mapped;
